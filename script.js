499ce62a21cab4e3ebbe42aa45d57cc74cf70312
Stephen Whitted <stephent.whitted@gmail.com>
2024-04-20 19:15:44 -0400

    part 1 underway

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
 console.log("Let's create some reusable functions!")
-/* ///////////////////////Functions Given////////////////////////////////////////////////////////////////////
-/**
- * 
- * Any of the examples below will accomplish the
- * same task: reversing a string.
- * 
- * Which of these examples is best? Why?
- * Note that there is no "correct" answer.
- 
-function reverseString(str) {
-    const strArray = str.split("");
-    const revArray = strArray.reverse();
-    const revString = revArray.join("");
-    return revString;
-  }
-  
-  function reverseString2(str) {
-    return str.split("").reverse().join("");
-  }
-  
-  function reverseString3(str) {
-    let revString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-      revString += str[i];
-    }
-    return revString;
-  }
-  
-  function reverseString4(str) {
-    if (str === "") return "";
-    else return reverseString4(str.substr(1)) + str.charAt(0);
-  }
-  
-  function reverseString5(str) {
-    return (str === "") ? "" : reverseString5(str.substr(1)) + str.charAt(0);
-  }
-  
-  console.log(reverseString("!dlroW olleH"))
-  console.log(reverseString2("!dlroW olleH"))
-  console.log(reverseString3("!dlroW olleH"))
-  console.log(reverseString4("!dlroW olleH"))
-  console.log(reverseString5("!dlroW olleH"))
-* 
-*/
+///////////////////////Reusable Functions ////////////////////////////////////////////////////////////////////
+
 ////////////////////////////Part 1///////////////////////////////////
+/*
+
+
+*
+
+*/
+// 1a Take an array of numbers and return the sum.
+//
+const numbers = [2, 4, 8, 16, 32, 64, 128, 256];
+
+
+function sumArray(numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+
+
+console.log(sumArray(numbers)); // Outputs: 510 as the sum
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+//1b Take an array of numbers and return the average.
+const numbers2 = [2, 4, 8, 16, 32, 64, 128, 256];
+
+
+function sumArray2(numbers2) {
+    return numbers2.reduce((acc, num) => acc + num, 0);
+}
+
+
+function avgArray2(numbers2) {
+    if (numbers2.length === 0) return 0;  
+    return sumArray2(numbers2) / numbers2.length;
+}
+
+const average = avgArray2(numbers2);
+console.log(average); //63.75 as the avg
+
+///////////////////////////////////////////////////////////////////////////
+
+//1c Take an array of strings and return the longest string.
+const str = ['please', 'wake', 'me', 'up', 'at', 'breakfast'];
+
+function longString(str) {
+    if (str.length === 0) return "";
+    let longest = str[0];
+    
+    for (let i = 1; i < str.length; i++) {
+        if (str[i].length > longest.length) {
+            longest = str[i]; 
+        }
+    }
+    
+    return longest; // 
+}
+
+const longestString = longString(str);
+console.log(longestString); // Outputs: "breakfast" as the longest string
+
+
+
+
+//Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+//For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+//Take a number, n, and print every number between 1 and n without using loops. Use recursion.
